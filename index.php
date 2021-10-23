2493ddd3d1b174b68d4df33e3881be993ff9029c
Garv Jain <garvj03@gmail.com>
2021-10-23 16:17:31 +0530

    Refactor File

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if(isset($_POST['login'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
